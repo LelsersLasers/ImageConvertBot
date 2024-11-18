bfcb3ef80e6eeab84918654529a8d3706359fcca
Millan Kumar <millankumar@gmail.com>
2024-11-18 10:03:05 -0500

    Back to supervisor

--- image_convert_bot/lib/image_convert_bot/application.ex
+++ image_convert_bot/lib/image_convert_bot/application.ex
@@ -4,10 +4,9 @@ defmodule ImageConvertBot.Application do
   def start(_type, _args) do
     IO.puts("Starting ImageConvertBot")
 
-    # children = [ImageConvertBot]
-    # IO.inspect(children)
-    # Supervisor.start_link(children, strategy: :one_for_one)
-    {:ok, pid} = ImageConvertBot.start_link(__MODULE__)
-    IO.puts(pid)
+    children = [ImageConvertBot]
+    IO.inspect(children)
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
