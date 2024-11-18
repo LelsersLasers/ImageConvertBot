960bccf3c9957464c45b0259f1ddad159e0e8d83
Millan Kumar <millankumar@gmail.com>
2024-11-17 21:36:47 -0500

    print statement

--- image_convert_bot/lib/image_convert_bot/application.ex
+++ image_convert_bot/lib/image_convert_bot/application.ex
@@ -1,8 +1,10 @@
 defmodule ImageConvertBot.Application do
-	use Application
+  use Application
 
-	def start(_type, _args) do
-	  children = [ImageConvertBot]
-	  Supervisor.start_link(children, strategy: :one_for_one)
-	end
+  def start(_type, _args) do
+    IO.puts("Starting ImageConvertBot")
+
+    children = [ImageConvertBot]
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
 end
