8239a1ed00640344dd41ec99975fb58395f65549
Millan Kumar <millankumar@gmail.com>
2024-11-18 13:05:28 -0500

    Can get the type we want to convert to + urls

--- image_convert_bot/lib/image_convert_bot.ex
+++ image_convert_bot/lib/image_convert_bot.ex
@@ -2,20 +2,34 @@ defmodule ImageConvertBot do
   use Nostrum.Consumer
 
   def handle_event({:MESSAGE_CREATE, msg, _state}) do
-    case msg.content do
-      "!convert" ->
+    if String.starts_with?(msg.content, "!convert") do
+      type =
+        msg.content
+        |> String.split(" ", parts: 2)
+        |> Enum.at(1)
+
+      if is_nil(type) do
         Nostrum.Api.create_message(
           msg.channel_id,
-          content: "Hello!",
-          message_reference: %{message_id: msg.id},
-      )
-      _ ->
-        :ignore
+          content: "Please provide a type to convert to!",
+          message_reference: %{message_id: msg.id}
+        )
+      else
+        # content = "Converting to #{type}!" <> " " <> msg.attachments |> Enum.map(& &1.url) |> Enum.join(", ")
+        urls =
+          msg.attachments
+          |> Enum.map(& &1.url)
+        Nostrum.Api.create_message(
+          msg.channel_id,
+          content: "Converting to #{type}!",
+          message_reference: %{message_id: msg.id}
+        )
+      end
     end
   end
 
   def handle_event({:READY, _event, _state}) do
-    IO.puts "Connected!"
+    IO.puts("Connected!")
   end
 
   def handle_event(_), do: :noop
