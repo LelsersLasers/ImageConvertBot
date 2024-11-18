dd141decb363399c16ba8e0780358c6cf95789fb
Millan Kumar <millankumar@gmail.com>
2024-11-18 09:55:04 -0500

    No supervisor?

--- image_convert_bot/lib/image_convert_bot/application.ex
+++ image_convert_bot/lib/image_convert_bot/application.ex
@@ -4,8 +4,10 @@ defmodule ImageConvertBot.Application do
   def start(_type, _args) do
     IO.puts("Starting ImageConvertBot")
 
-    children = [ImageConvertBot]
-    IO.inspect(children)
-    Supervisor.start_link(children, strategy: :one_for_one)
+    # children = [ImageConvertBot]
+    # IO.inspect(children)
+    # Supervisor.start_link(children, strategy: :one_for_one)
+    {:ok, pid} = ImageConvertBot.start_link(__MODULE__)
+    IO.puts(pid)
   end
 end
