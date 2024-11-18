fe2d5510553af75da6cca92eed9151384743217b
Millan Kumar <millankumar@gmail.com>
2024-11-18 08:54:00 -0500

    Debug print

--- image_convert_bot/lib/image_convert_bot/application.ex
+++ image_convert_bot/lib/image_convert_bot/application.ex
@@ -5,6 +5,7 @@ defmodule ImageConvertBot.Application do
     IO.puts("Starting ImageConvertBot")
 
     children = [ImageConvertBot]
+    IO.inspect(children)
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
