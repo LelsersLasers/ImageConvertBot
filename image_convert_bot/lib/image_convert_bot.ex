1f3d76c205e3ad34a864f409e9edd377259e886b
Millan Kumar <millankumar@gmail.com>
2024-11-18 11:38:13 -0500

    READY and MESSAGE_CREATE commands

--- image_convert_bot/lib/image_convert_bot.ex
+++ image_convert_bot/lib/image_convert_bot.ex
@@ -1,9 +1,20 @@
 defmodule ImageConvertBot do
   use Nostrum.Consumer
 
-  def handle_event(event) do
-    IO.inspect event
+  alias Nostrum.Api
 
-    :noop
+  def handle_event({:MESSAGE_CREATE, msg, _state}) do
+    case msg.content do
+      "!ping" ->
+        Api.create_message(msg.channel_id, "Pong!")
+      _ ->
+        :ignore
+    end
   end
+
+  def handle_event({:READY, _event, _state}) do
+    IO.puts "Connected!"
+  end
+
+  def handle_event(_), do: :noop
 end
