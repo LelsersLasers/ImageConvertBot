aabe995548af5b5dc546df7177c07acd300817fa
Millan Kumar <millankumar@gmail.com>
2024-11-18 10:07:38 -0500

    new proj

---
+++ image_convert_bot/lib/image_convert_bot.ex
@@ -0,0 +1,18 @@
+defmodule ImageConvertBot do
+  @moduledoc """
+  Documentation for `ImageConvertBot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ImageConvertBot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ image_convert_bot/lib/image_convert_bot/application.ex
@@ -0,0 +1,20 @@
+defmodule ImageConvertBot.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: ImageConvertBot.Worker.start_link(arg)
+      # {ImageConvertBot.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ImageConvertBot.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
