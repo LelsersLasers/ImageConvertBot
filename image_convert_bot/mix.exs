938964f42b85c32b3329ee1e584249a827e5ab40
Millan Kumar <millankumar@gmail.com>
2024-11-18 08:51:32 -0500

    Adding entry point??

--- image_convert_bot/mix.exs
+++ image_convert_bot/mix.exs
@@ -14,6 +14,7 @@ defmodule ImageConvertBot.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {ImageConvertBot.Application, []},
       extra_applications: [:logger]
     ]
   end
